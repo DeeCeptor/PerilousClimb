a544e5a3601c00c09de0a1d16a7a3b521e04e384
Squirrell <ksquires11@gmail.com>
2015-12-02 22:17:44 -0600

    rope stuff still

--- Climbers/Assets/Script/RopeAttachScript.cs
+++ Climbers/Assets/Script/RopeAttachScript.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class RopeAttachScript : MonoBehaviour {
-
+    public int layerToHit = 11;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +12,14 @@ public class RopeAttachScript : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == layerToHit)
+        {
+            HingeJoint2D newHinge = gameObject.AddComponent<HingeJoint2D>();
+            newHinge.connectedAnchor = collision.contacts[0].point;
+            newHinge.anchor = new Vector2(0,0);
+        }
+    }
 }

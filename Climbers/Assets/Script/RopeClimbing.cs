0c45e37b505d1a9e99e4f689f3d6048b2c6411ed
DeeCeptor <sirmichaellong@gmail.com>
2015-12-02 23:20:27 -0600

    Rope swinging!

---
+++ Climbers/Assets/Script/RopeClimbing.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class RopeClimbing : MonoBehaviour
+{
+    public Transform current_segment;
+    public Transform above_segment;
+    float climbing_speed = 0.3f;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Let go of the rope
+            this.GetComponent<PlatformerCharacter2D>().enabled = true;
+            this.GetComponent<Rigidbody2D>().isKinematic = false;
+            this.transform.parent = null;
+        }
+	}
+
+
+    void FixedUpdate()
+    {
+        // If at the next segment, find the next segment
+        if (((Vector2) this.transform.position - (Vector2) above_segment.transform.position).magnitude < 0.1f)
+        {
+            Debug.Log("at same segment");
+            this.current_segment = above_segment;
+            above_segment = current_segment.GetComponent<HingeJoint2D>().connectedBody.transform;
+            this.transform.parent = this.current_segment.transform;
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // Moving up
+        if (v > 0)
+        {
+            this.transform.position = Vector2.Lerp(this.transform.position, above_segment.transform.position, climbing_speed);
+        }
+
+        // Swing rope left and right
+        if (h != 0)
+        {
+            current_segment.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(h * -500, 0));
+        }
+    }
+}
---
+++ Climbers/Assets/Script/RopeSegment.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class RopeSegment : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" 
+            && (other.gameObject.GetComponent<RopeClimbing>() == null
+            || other.gameObject.GetComponent<RopeClimbing>().enabled))
+        {
+            other.gameObject.transform.parent = this.transform;
+            other.GetComponent<Rigidbody2D>().isKinematic = true;
+            other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            //other.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+            RopeClimbing r = other.gameObject.GetComponent<RopeClimbing>();
+            if (other.gameObject.GetComponent<RopeClimbing>() == null)
+                r = other.gameObject.AddComponent<RopeClimbing>();
+            else
+                r.enabled = true;
+
+            r.current_segment = this.transform;
+            r.above_segment = this.GetComponent<HingeJoint2D>().connectedBody.transform;
+            other.GetComponent<PlatformerCharacter2D>().enabled = false;
+        }
+    }
+}

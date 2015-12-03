0bcc171cc0e65809a7ff527ab7412e935a3dcee0
DeeCeptor <sirmichaellong@gmail.com>
2015-12-03 15:34:27 -0600

    Pixel perfect camera

---
+++ Climbers/Assets/Script/CameraMoveByPixel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMoveByPixel : MonoBehaviour
+{
+    public Transform object_to_follow;  // Object we are tracking
+    private Camera this_camera;
+    private float starting_z;
+
+
+	void Start ()
+    {
+        this_camera = this.GetComponent<Camera>();
+        starting_z = this.transform.position.z;
+	}
+
+
+    void LateUpdate ()
+    {
+        this.transform.position = new Vector3(RoundToNearestPixel(object_to_follow.transform.position.x, this_camera),
+                                              RoundToNearestPixel(object_to_follow.transform.position.y, this_camera),
+                                              starting_z);
+	}
+
+
+    public static float RoundToNearestPixel(float unityUnits, Camera viewingCamera)
+    {
+        float valueInPixels = (Screen.height / (viewingCamera.orthographicSize * 2)) * unityUnits;
+        valueInPixels = Mathf.Round(valueInPixels);
+        float adjustedUnityUnits = valueInPixels / (Screen.height / (viewingCamera.orthographicSize * 2));
+        return adjustedUnityUnits;
+    }
+}
--- Climbers/Assets/Script/FollowObject.cs
+++ Climbers/Assets/Script/FollowObject.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class FollowObject : MonoBehaviour
 {
     public Transform object_to_follow;
+    private float starting_z;
+
+    void Start()
+    {
+        starting_z = this.transform.position.z;
+    }
+
 
     void Update ()
     {
-        this.transform.position = object_to_follow.position;
+        this.transform.position = new Vector3(object_to_follow.position.x, object_to_follow.position.y, starting_z);
     }
 }

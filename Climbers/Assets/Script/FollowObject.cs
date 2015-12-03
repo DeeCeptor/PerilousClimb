7c4611d18ad4a3a856ea30dec44090227803f218
DeeCeptor <sirmichaellong@gmail.com>
2015-12-02 21:42:30 -0600

    Added aiming line

---
+++ Climbers/Assets/Script/FollowObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform object_to_follow;
+
+    void Update ()
+    {
+        this.transform.position = object_to_follow.position;
+    }
+}

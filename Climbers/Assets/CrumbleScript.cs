270371ab7db0770810a07d43a0b4ee2c7350f4ec
Squirrell <ksquires11@gmail.com>
2015-12-15 15:25:49 -0600

    spikes and crumbling stuff still need to get snow to look good

---
+++ Climbers/Assets/CrumbleScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrumbleScript : MonoBehaviour {
+    //This script is for crumbling blocks they will become a bunch of debris which will damage the player if the debris falls on them
+    public GameObject debris;
+    public int debrisNumber;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Climbers/Assets/SpikeScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeScript : MonoBehaviour {
+    //determines the speed at witch the player must be falling to take damage(should be a positive value for falling
+    public float fallthreshold = 5f;
+    public float damage = 1f;
+    public int playerLayer = 8;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    void Update()
+    {
+    }
+	
+	void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == playerLayer && collision.gameObject.GetComponent<Rigidbody2D>().velocity.y < -fallthreshold && collision.GetType() == typeof(CircleCollider2D))
+        {
+            PlatformerCharacter2D playerScript = collision.gameObject.GetComponent<PlatformerCharacter2D>();
+            playerScript.AdjustHP(-damage);
+            Debug.Log("hit");
+        }
+    }
+}
